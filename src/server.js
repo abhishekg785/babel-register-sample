9a0ee85518106ba6b1bb50966461fba499cb60da
abhishek <abhishek.goswami@pipedrive.com>
2019-12-18 15:19:58 +0200

    babel register usage

---
+++ src/server.js
@@ -0,0 +1,20 @@
+/**
+ * import should work
+ */
+import fs from 'fs';
+
+/**
+ * jsx should work
+ */
+import React from 'react';
+const jsx = (<div></div>);
+
+
+function start() {
+    // some server stuff
+    console.log('started')
+}
+
+module.exports = {
+    start,
+}
